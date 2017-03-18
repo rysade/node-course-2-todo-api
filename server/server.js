e3f1c628e26b746a0f8246b38113cefa3193ed39
rysade <rysade@gmail.com>
2017-03-18 10:32:01 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    "use strict";
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
 
 app.listen(3000, () => {
     "use strict";
